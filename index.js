69b97846e73581eb6646da76077576a05a6c5363
+ <+>
2023-02-10 20:02:29 -0700

    Array method lab complete

--- index.js
+++ index.js
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (string) {
+    let eachIt = string.split(' ')
+    let newArr = []
+    for (let i = 0; i < eachIt.length; i++) {
+      let word = eachIt[i];
+      let capitalFirst = word.charAt(0).toUpperCase()
+      let restLower = word.slice(1)
+      newArr.push(capitalFirst + restLower)
+    } return newArr.join(' ')
+  })
 }
+
